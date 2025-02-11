db897af61021526557e70edec176bfd8e10ee8be
manatsx <manashe.tsx@gmail.com>
2025-02-11 15:54:06 -0300

    middlewares en zustand

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -2,6 +2,7 @@ import { type StateCreator, create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { customSessionStorage } from "../storages/session.storage";
 // import { firebaseStorage } from "../storages/firebase.storage";
+// import { logger } from "../middlewares/logger.middleware";
 
 interface PersonState {
   firstName: string;
@@ -31,7 +32,7 @@ export const usePersonStore = create<PersonState & Actions>()(
     persist(storeAPi, {
       name: "person-storage",
       storage: customSessionStorage,
-      //   storage: firebaseStorage,
+      // storage: firebaseStorage,
     })
   )
 );
